4f42fd13afcc47d4bd220fbd1bb9668b25bbbea3
madalinaghica12 <117913493+madalinaghica12@users.noreply.github.com>
2026-04-26 11:35:30 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+const PORT = 3000;
+
+app.get('/api/data', (req, res) => {
+    fs.readFile('data.json', 'utf8', (err, data) => {
+        if (err) {
+            return res.status(500).json({ error: 'Cannot read file' });
+        }
+        res.json(JSON.parse(data));
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
